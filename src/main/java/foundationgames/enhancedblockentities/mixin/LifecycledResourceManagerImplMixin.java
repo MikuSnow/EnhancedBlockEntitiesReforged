f1e5a30d0d11ce0c267f597407e693c14e68cf03
千飞夏 <gal499605076@gmail.com>
2023-12-04 00:40:48 +0800

    1.18.1支持

--- src/main/java/foundationgames/enhancedblockentities/mixin/LifecycledResourceManagerImplMixin.java
+++ src/main/java/foundationgames/enhancedblockentities/mixin/LifecycledResourceManagerImplMixin.java
@@ -17,56 +17,56 @@
  */
 package foundationgames.enhancedblockentities.mixin;
 
+import com.google.common.collect.Lists;
 import foundationgames.enhancedblockentities.util.EBEUtil;
 import foundationgames.enhancedblockentities.util.ResourceUtil;
 import foundationgames.enhancedblockentities.util.hacks.ExperimentalSetup;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.FallbackResourceManager;
-import net.minecraft.server.packs.resources.MultiPackResourceManager;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimpleReloadableResourceManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@Mixin(MultiPackResourceManager.class)
+@Mixin(SimpleReloadableResourceManager.class)
 public abstract class LifecycledResourceManagerImplMixin {
     @Shadow
     @Final
-    private Map<String, FallbackResourceManager> namespacedManagers;
+    private Map<String, FallbackResourceManager> namespacedPacks;
 
-    @ModifyVariable(method = "<init>", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;copyOf(Ljava/util/Collection;)Ljava/util/List;", shift = At.Shift.BEFORE), ordinal = 0)
-    private List<PackResources> enhanced_bes$injectRRP(List<PackResources> old) {
-        var packs = new ArrayList<>(old);
+    @Shadow @Final private List<PackResources> packs = Lists.newArrayList();;
+
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    public void enhanced_bes$injectRRP(PackType p_10878_, CallbackInfo ci) {
+        var pack = new ArrayList<>(packs);
 
         int idx = 0;
-        if (packs.size() > 0) do {
+        if (pack.size() > 0) do {
             idx++;
-        } while (idx < packs.size() && !EBEUtil.isVanillaResourcePack(packs.get(idx - 1)));
-        packs.add(idx, ResourceUtil.getPack());
-
-        return packs;
+        } while (idx < pack.size() && !EBEUtil.isVanillaResourcePack(pack.get(idx - 1)));
+        pack.add(idx, ResourceUtil.getPack());
     }
 
     @Inject(method = "<init>", at = @At(value = "TAIL"))
-    private void enhanced_bes$injectExperimentalPack(PackType type, List<PackResources> packs, CallbackInfo ci) {
+    private void enhanced_bes$injectExperimentalPack(PackType p_10878_, CallbackInfo ci) {
         ExperimentalSetup.cacheResources((ResourceManager) this);
         ExperimentalSetup.setup();
 
-        addPack(type, ResourceUtil.getExperimentalPack());
+        addPack(p_10878_, ResourceUtil.getExperimentalPack());
     }
 
     private void addPack(PackType type, PackResources pack) {
         for (var namespace : pack.getNamespaces(type)) {
-            this.namespacedManagers.computeIfAbsent(namespace, n -> new FallbackResourceManager(type, n)).add(pack);
+            this.namespacedPacks.computeIfAbsent(namespace, n -> new FallbackResourceManager(type, n)).add(pack);
         }
     }
 }
